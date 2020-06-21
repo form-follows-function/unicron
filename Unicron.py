ca688067b8fd1c77db32aafb7bcce62857ba592e
Nelson Sylvest*r Fritsch <info@nelsonfritsch.de>
2020-06-21 16:34:30 +0200

    Add preferences window

--- Unicron.py
+++ Unicron.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
+from Cocoa import objc
 from AppKit import NSApplication, NSApp, NSWindowController
 from PyObjCTools import AppHelper
 from main import Unicron
 
 class UnicronController(NSWindowController):
-    Unicron()
+    def windowDidLoad(self):
+        NSWindowController.windowDidLoad(self)
+        self.Unicron = Unicron()
+
+    @objc.IBAction
+    def openGithub_(self, sender):
+        import webbrowser
+        webbrowser.open('https://github.com/form-follows-function/unicron', new=2)
+
+    @objc.IBAction
+    def showPrefs_(self, sender):
+        self.Unicron.prefsWindow.open()
+
         
 if __name__ == "__main__":
     app = NSApplication.sharedApplication()
